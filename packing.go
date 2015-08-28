e5440dc396773647e17c571aa0476f0d41aff61a
John Chadwick <johnwchadwick@gmail.com>
2015-08-27 22:26:46 -0400

    Improve documentation.

--- packing.go
+++ packing.go
@@ -25,7 +25,35 @@ import (
 	"reflect"
 )
 
-// Unpack reads data from a byteslice into a structure.
+/*
+Unpack reads data from a byteslice into a structure.
+
+Each structure field will be read sequentially based on a straightforward
+interpretation of the type. For example, an int32 will be read as a 32-bit
+signed integer, taking 4 bytes of memory. Structures and arrays are laid out
+flat with no padding or metadata.
+
+The behavior of deserialization can be customized using struct tags. The
+following struct tag syntax is supported:
+
+	`struct:"[flags...]"`
+
+Flags are comma-separated keys. The following are available:
+
+	type            A bare type name, e.g. int32 or []string.
+
+	sizeof=[Field]  Specifies that the field should be treated as a count of
+	                the number of elements in Field.
+
+	skip=[Count]    Skips Count bytes before the field. You can use this to
+	                e.g. emulate C structure alignment.
+
+	big,msb         Specifies big endian byte order. When applied to structs,
+	                this will apply to all fields under the struct.
+
+	little,lsb      Specifies little endian byte order. When applied to structs,
+	                this will apply to all fields under the struct.
+*/
 func Unpack(data []byte, order binary.ByteOrder, v interface{}) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -40,7 +68,12 @@ func Unpack(data []byte, order binary.ByteOrder, v interface{}) (err error) {
 	return
 }
 
-// Pack writes data from a datastructure into a byteslice.
+/*
+Pack writes data from a datastructure into a byteslice.
+
+Each structure is serialized in the same way it would be deserialized with
+Unpack. See Unpack documentation for the struct tag format.
+*/
 func Pack(order binary.ByteOrder, v interface{}) (data []byte, err error) {
 	defer func() {
 		if r := recover(); r != nil {
